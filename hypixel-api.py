6aff77ffd46635641f1f3694fbe07b2228f2a7e0
a1exsDev <117765022+a1exsDev@users.noreply.github.com>
2024-01-01 06:58:23 +0000

    I pull push doors.

--- hypixel-api.py
+++ hypixel-api.py
@@ -1,22 +1,32 @@
 import requests
 import json
-from pprint import pp, pprint
+import os
+from pprint import pprint
+
 
 def getInfo(call):
-    r = requests.get(call)
-    return r.json()
+  r = requests.get(call)
+  return r.json()
+
 
 def convert_to_uuid():
-    return getInfo(f"https://api.mojang.com/users/profiles/minecraft/{player_name}").get("id")
+  return getInfo(f"https://api.mojang.com/users/profiles/minecraft/{player_name}").get("id")
 
-API_KEY = "c0b5fc9f-7432-4807-a996-cb1a2e3785a9"
+
+API_KEY = os.environ.get("API_KEY")
 
 player_name = input("Player Name: ")
 player_uuid = convert_to_uuid()
 
-uuid_convert = f"https://api.mojang.com/users/profiles/minecraft/{player_name}"
-skyblock_link = f"https://api.hypixel.net/v2/skyblock/profile?key={API_KEY}&uuid={player_uuid}"
+skyblock_link = f"https://api.hypixel.net/v2/skyblock/profiles?key={API_KEY}&uuid={player_uuid}"
 
+if (player_uuid == "None"):
+  print("Username not found.")
+  exit()
+elif (getInfo(skyblock_link).get("profiles") == None):
+  print("No profile found.")
+  exit()
 
+print(player_uuid)
 pprint(getInfo(skyblock_link))
 print(skyblock_link)
